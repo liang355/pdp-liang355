f19de70b51161b103c5c936f5bc9f59e64dc7208
Ben Liang <liang355@umn.edu>
2017-09-12 17:57:42 -0700

    assignment1

---
+++ src/main/java/Assignment1/RandomHomework.java
@@ -0,0 +1,21 @@
+package Assignment1;
+
+import java.util.*;
+
+public class RandomHomework {
+    public static void main(String [] argv) {
+        RandomHomework randomHomework = new RandomHomework();
+        System.out.println(randomHomework.getCourse());
+    }
+
+    private String getCourse () {
+        List<String> courses = new ArrayList<String>();
+        courses.add("Programming Design Paradigms");
+        courses.add("Object Oriented Design");
+        courses.add("Data Structures and Algorithms");
+        courses.add("Data Mining");
+
+        Random rand = new Random();
+        return courses.get(rand.nextInt(4));
+    }
+}
