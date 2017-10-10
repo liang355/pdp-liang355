86a3f89ca3da1cc488a8247aa822019653f39eb0
Ben Liang <liang355@umn.edu>
2017-10-09 17:57:16 -0700

    tests

--- Assignment3/src/test/java/edu/neu/ccs/cs5010/TemplateEditorTest.java
+++ Assignment3/src/test/java/edu/neu/ccs/cs5010/TemplateEditorTest.java
@@ -1,5 +1,8 @@
 package edu.neu.ccs.cs5010;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import org.junit.Test;
@@ -15,5 +18,25 @@ public class TemplateEditorTest {
     @Test
     public void testInitialState() {
         editor = new TemplateEditor(templatePathname);
+        String expectedTemplateString;
+        FileReader inputFile = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            inputFile = new FileReader(templatePathname);
+            int charInt;
+            while ((charInt = inputFile.read()) != -1) {
+                sb.append((char)charInt);
+            }
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("*** OOPS! A file was not found : " + fnfe.getMessage());
+            fnfe.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println("Something went wrong! : " + ioe.getMessage());
+            ioe.printStackTrace();
+        }
+
+        expectedTemplateString = sb.toString();
+        String actualTemplateString = editor.getTemplateString();
+        assertEquals(actualTemplateString, expectedTemplateString);
     }
 }
