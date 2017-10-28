79cfa48d02fdc836600921f4177ad9510abde09c
Ben Liang <liang355@umn.edu>
2017-10-28 06:47:28 -0700

    Assignment4

--- Assignment4/src/main/java/edu/neu/ccs/cs5010/CSVReader.java
+++ Assignment4/src/main/java/edu/neu/ccs/cs5010/CSVReader.java
@@ -1,58 +1,97 @@
 package edu.neu.ccs.cs5010;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.regex.*;
 
 public class CSVReader {
-    private List<Candy> desiredCandies = new ArrayList<>();
-    private int numOfCSVs;
-    private String[] CSVNames;
 
-    public CSVReader(int numOfCSVs, String[] CSVNames) {
-        this.numOfCSVs = numOfCSVs;
-        this.CSVNames = CSVNames;
+    private List<Candy> readLineToCandies(String line) {
+        // get raw candy strings
+        String[] candyStrings = line.split(", ");
+        List<Candy> desiredCandies = new ArrayList<>();
+        Pattern p;
+        Matcher m;
+        String size;
+        String type;
+        // iterate over raw candy strings
+        for(String s : candyStrings) {
+            // match and get size:
+            p = Pattern.compile("(super size|king size|fun size).*?");
+            m = p.matcher(s.toLowerCase());
+            if(m.matches()) {
+                size = m.group(1);
+                String[] parts = size.split(" ");
+                size = parts[0].substring(0, 1).toUpperCase() + parts[0].substring(1) + " " +
+                        parts[1].substring(0, 1).toUpperCase() + parts[1].substring(1);
+            } else {
+                size = "Regular Size";
+            }
+            // match and get type:
+            p = Pattern.compile(".*?(twix|snickers|mars|kit kat|whoopers|milky way|toblerone|crunch|baby ruth|almond joy)");
+            m = p.matcher(s.toLowerCase());
+            if(m.matches()) {
+                type = m.group(1);
+                String[] parts = type.split(" ");
+                for(int i = 0; i < parts.length; i++) {
+                    StringBuilder sb = new StringBuilder("");
+                    sb.append(parts[i].substring(0, 1).toUpperCase()).append(parts[i].substring(1));
+                    parts[i] = sb.toString();
+                }
+                type = String.join(" ", parts);
+            } else {
+                type = "Unrecognized Candy";
+            }
+            // create Candy(size, type)
+            desiredCandies.add(new Candy(size, type));
+        }
+        System.out.println(Arrays.toString(desiredCandies.toArray()));
+        return desiredCandies;
     }
 
-    public List<Candy> getDesiredCandiesFromCSV() {
+    public List<Candy> getDesiredCandiesFromCSV(String CSVPathname) {
         // read CSV here..
-//        BufferedReader inputFile = null;
-//        try {
-//            inputFile = new BufferedReader(new FileReader(CSVPathname));
-//            String line;
-//
-//            if((line = inputFile.readLine()) != null) {
-//                String[] candyStrings = line.split(", ");
-//
-//            }
-//        } catch (FileNotFoundException fnfe) {
-//            System.out.println("*** OOPS! A file was not found : " + fnfe.getMessage());
-//            fnfe.printStackTrace();
-//        } catch (IOException ioe) {
-//            System.out.println("Something went wrong! : " + ioe.getMessage());
-//            ioe.printStackTrace();
-//        } finally {
-//            if (inputFile != null) {
-//                try {
-//                    inputFile.close();
-//                } catch (IOException e) {
-//                    System.out.println("Failed to close input stream in finally block");
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
+        List<Candy> desiredCandies = new ArrayList<>();
+        BufferedReader inputFile = null;
+        try {
+            inputFile = new BufferedReader(new FileReader(CSVPathname));
+            String line;
+
+            if((line = inputFile.readLine()) != null) {
+                return readLineToCandies(line);
+            }
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("*** OOPS! A file was not found : " + fnfe.getMessage());
+            fnfe.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println("Something went wrong! : " + ioe.getMessage());
+            ioe.printStackTrace();
+        } finally {
+            if (inputFile != null) {
+                try {
+                    inputFile.close();
+                } catch (IOException e) {
+                    System.out.println("Failed to close input stream in finally block");
+                    e.printStackTrace();
+                }
+            }
+        }
+        return desiredCandies;
+    }
+}
 
 
-        desiredCandies.add(new Candy("Super Size", "Twix"));
-        desiredCandies.add(new Candy("Regular Size", "Snickers"));
-        desiredCandies.add(new Candy("King Size", "Mars"));
-        desiredCandies.add(new Candy("Regular Size", "Kit Kat"));
-        desiredCandies.add(new Candy("Fun Size", "Toblerone"));
-        desiredCandies.add(new Candy("Regular Size", "Almond Joy"));
-        desiredCandies.add(new Candy("Fun Size", "Baby Ruth"));
-        desiredCandies.add(new Candy("Super Size", "Milky Way"));
-        desiredCandies.add(new Candy("King Size", "Mars"));
-        desiredCandies.add(new Candy("Regular Size", "Twix"));
+
+//        desiredCandies.add(new Candy("Super Size", "Twix"));
+//        desiredCandies.add(new Candy("Regular Size", "Snickers"));
+//        desiredCandies.add(new Candy("King Size", "Mars"));
+//        desiredCandies.add(new Candy("Regular Size", "Kit Kat"));
+//        desiredCandies.add(new Candy("Fun Size", "Toblerone"));
+//        desiredCandies.add(new Candy("Regular Size", "Almond Joy"));
+//        desiredCandies.add(new Candy("Fun Size", "Baby Ruth"));
+//        desiredCandies.add(new Candy("Super Size", "Milky Way"));
+//        desiredCandies.add(new Candy("King Size", "Mars"));
+//        desiredCandies.add(new Candy("Regular Size", "Twix"));
 
 //        desiredCandies.add(new Candy("Super Size", "Twix"));
 //        desiredCandies.add(new Candy("Regular Size", "Snickers"));
@@ -64,6 +103,3 @@ public class CSVReader {
 //        desiredCandies.add(new Candy("Super Size", "Milky Way"));
 //        desiredCandies.add(new Candy("King Size", "Mars"));
 //        desiredCandies.add(new Candy("Regular Size", "Crunch"));
-        return this.desiredCandies;
-    }
-}
--- Assignment4/src/main/java/edu/neu/ccs/cs5010/HalloweenNeighborhoodTraversal.java
+++ Assignment4/src/main/java/edu/neu/ccs/cs5010/HalloweenNeighborhoodTraversal.java
@@ -5,24 +5,33 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HalloweenNeighborhoodTraversal {
     private List<Visitable> households;
-    private List<Candy> desiredCandies;
     private String traversalString = "Candy type, Candy size, Household type\n";
     private int traversalCount = 0;
+    private String curCSVName = "";
 
-    private HalloweenNeighborhoodTraversal(List<Candy> desiredCandies) {
+    private HalloweenNeighborhoodTraversal() {
         households = new ArrayList<>();
         households.add(new HouseholdDetachedHouse());
         households.add(new HouseholdDuplex());
         households.add(new HouseholdMansion());
         households.add(new HouseholdTownHome());
-        this.desiredCandies = desiredCandies;
     }
 
     private void printTraversalToFile(String traversalString) {
-        String filePathname = "DreamTraversal" + ++traversalCount + ".txt";
+        // make output file name
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(curCSVName);
+        String cnt = "";
+        if(m.find()) {
+            cnt = m.group();
+        }
+        String filePathname = "DreamTraversal" + cnt + ".txt";
+        // use PrintWriter
         try {
             PrintWriter out = new PrintWriter(filePathname);
             out.print(traversalString);
@@ -48,7 +57,7 @@ public class HalloweenNeighborhoodTraversal {
         return visitor.hasDesiredCandy();
     }
 
-    public void findAndPrintTraversal() {
+    private void findAndPrintTraversalForThisList(List<Candy> desiredCandies) {
         for(Candy c : desiredCandies) {
             if(!findThisCandy(c)) {
                 System.out.println("No traversal exists");
@@ -59,12 +68,28 @@ public class HalloweenNeighborhoodTraversal {
         printTraversalToFile(this.traversalString);
     }
 
+    public void findAndPrintAllTraversals(int numOfCSVs, String[] CSVNames) {
+        CSVReader csvReader = new CSVReader();
+        for(int i = 0; i < numOfCSVs; i++) {
+            this.curCSVName = CSVNames[i];
+            List<Candy> candies = csvReader.getDesiredCandiesFromCSV(CSVNames[i]);
+            findAndPrintTraversalForThisList(candies);
+        }
+    }
+
     public static void main(String[] args) {
         int numOfCSVs = 2;
-        String[] CSVNames = new String[]{"DreamCandy1", "DreamCandy2"};
-        CSVReader csvReader = new CSVReader(numOfCSVs, CSVNames);
-        HalloweenNeighborhoodTraversal traversal = new HalloweenNeighborhoodTraversal(csvReader.getDesiredCandiesFromCSV());
+        String[] CSVNames = new String[] {"DreamCandy1.csv", "DreamCandy2.csv"};
 
-        traversal.findAndPrintTraversal();
+        if(args.length != 0) {
+            numOfCSVs = Integer.parseInt(args[0]);
+            CSVNames = new String[args.length - 1];
+            for(int i = 1; i < args.length; i++) {
+                CSVNames[i - 1] = args[i];
+            }
+        }
+        
+        HalloweenNeighborhoodTraversal traversal = new HalloweenNeighborhoodTraversal();
+        traversal.findAndPrintAllTraversals(numOfCSVs, CSVNames);
     }
 }
